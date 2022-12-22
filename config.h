bb2f58ecd4b856e63e9694d5fab576fa08ac8b13
Sam Mitchell <jfetkotto@gmail.com>
2022-12-22 19:13:18 +0000

    Papercolor-theme dark

--- config.h
+++ config.h
@@ -95,22 +95,22 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-        "#101010", /* base00 */
-        "#7c7c7c", /* base08 */
-        "#8e8e8e", /* base0B */
-        "#a0a0a0", /* base0A */
-        "#686868", /* base0D */
-        "#747474", /* base0E */
-        "#868686", /* base0C */
-        "#b9b9b9", /* base05 */
-        "#525252", /* base03 */
-        "#999999", /* base09 */
-        "#252525", /* base01 */
-        "#464646", /* base02 */
-        "#ababab", /* base04 */
-        "#e3e3e3", /* base06 */
-        "#5e5e5e", /* base0F */
-        "#f7f7f7", /* base07 */
+        "#1c1c1c",
+        "#af005f",
+        "#5faf00",
+        "#d7af5f",
+        "#5fafd7",
+        "#808080",
+        "#d7875f",
+        "#d0d0d0",
+        "#585858",
+        "#5faf5f",
+        "#afd700",
+        "#af87d7",
+        "#ffaf00",
+        "#ff5faf",
+        "#00afaf",
+	"#5f8787",
 };
 
 
@@ -190,8 +190,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-        { ShiftMask,            XK_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Down,   kscrolldown,    {.i = -1} },
+        { ShiftMask,            XK_Up,          kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Down,        kscrolldown,    {.i = -1} },
 };
 
 /*
