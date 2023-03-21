4ea20b6d69bdf079774ef7a2494e95624abe939b
Sam Mitchell <jfetkotto@gmail.com>
2023-03-21 10:29:08 +0000

    base16-black-metal-burzum

--- config.h
+++ config.h
@@ -95,28 +95,28 @@ unsigned int tabspaces = 8;
 
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#51576D",
-	"#E78284",
-	"#A6D189",
-	"#E5C890",
-	"#8CAAEE",
-	"#F4B8E4",
-	"#81C8BE",
-	"#B5BFE2",
+	"#000000",
+	"#121212",
+	"#222222",
+	"#333333",
+	"#999999",
+	"#c1c1c1",
+	"#999999",
+	"#c1c1c1",
 
 	/* 8 bright colors */
-	"#626880",
-	"#E78284",
-	"#A6D189",
-	"#E5C890",
-	"#8CAAEE",
-	"#F4B8E4",
-	"#81C8BE",
-	"#A5ADCE",
+	"#5f8787",
+	"#aaaaaa",
+	"#99bbaa",
+	"#ddeecc",
+	"#aaaaaa",
+	"#888888",
+	"#999999",
+	"#444444",
 
-	[256] = "#C6D0F5", /* default foreground colour */
-	[257] = "#303446", /* default background colour */
-	[258] = "#F2D5CF", /*575268*/
+	[256] = "#c1c1c1", /* default foreground colour */
+	[257] = "#000000", /* default background colour */
+	[258] = "#999999", /*575268*/
 };
 
 
