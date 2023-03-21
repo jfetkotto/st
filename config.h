0744b9f4352d013b27f5956656d2f16622168882
Sam Mitchell <jfetkotto@gmail.com>
2023-03-21 16:00:54 +0000

    Everforest

--- config.h
+++ config.h
@@ -95,28 +95,28 @@ unsigned int tabspaces = 8;
 
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#51576D",
-	"#E78284",
-	"#A6D189",
-	"#E5C890",
-	"#8CAAEE",
-	"#F4B8E4",
-	"#81C8BE",
-	"#B5BFE2",
+	"#4b565c",
+	"#e67e80",
+	"#a7c080",
+	"#dbbc7f",
+	"#7fbbb3",
+	"#d699b6",
+	"#83c092",
+	"#d3c6aa",
 
 	/* 8 bright colors */
-	"#626880",
-	"#E78284",
-	"#A6D189",
-	"#E5C890",
-	"#8CAAEE",
-	"#F4B8E4",
-	"#81C8BE",
-	"#A5ADCE",
+	"#4b565c",
+	"#e67e80",
+	"#a7c080",
+	"#dbbc7f",
+	"#7fbbb3",
+	"#d699b6",
+	"#83c092",
+	"#d3c6aa",
 
-	[256] = "#C6D0F5", /* default foreground colour */
-	[257] = "#303446", /* default background colour */
-	[258] = "#F2D5CF", /*575268*/
+	[256] = "#d3c6aa", /* default foreground colour */
+	[257] = "#2f383e", /* default background colour */
+	[258] = "#d3c6aa", /*575268*/
 };
 
 
